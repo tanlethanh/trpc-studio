47783f1931555419c455994cd5168fb8bd54e587
Tan Le <104194494+tanlethanh@users.noreply.github.com>
2025-05-17 22:10:52 +0700

    refactor: simplify ThemeProvider component by removing redundant props

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,23 +2,8 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      themes={["light", "dark"]}
-      value={{
-        light: "light",
-        dark: "dark",
-      }}
-      {...props}
-    >
-      {children}
-    </NextThemesProvider>
-  )
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
