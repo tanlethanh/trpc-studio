131008e6f0177530930db92c23ad5f480f82492c
Tan Le <104194494+tanlethanh@users.noreply.github.com>
2025-05-18 09:26:18 +0700

    fix: update default theme in layout to dark and disable system theme switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
