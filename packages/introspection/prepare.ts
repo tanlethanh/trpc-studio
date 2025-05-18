b7ffacc48056dfd11d23aaa54abe621c98d43857
Tan Le <104194494+tanlethanh@users.noreply.github.com>
2025-05-18 08:33:20 +0700

    chore: update package.json and prepare script for introspection package

--- packages/introspection/prepare.ts
+++ packages/introspection/prepare.ts
@@ -7,6 +7,8 @@ const distPath = join(__dirname, 'dist', 'README.md');
 
 const readme = await readFile(readmePath, 'utf-8');
 await writeFile(distPath, readme);
+console.log('Copied README.md to dist');
+
 
 // Copy package.json with change entries to ./index.js and ./index.d.ts
 const packageJsonPath = join(__dirname, 'package.json');
@@ -16,5 +18,11 @@ packageJsonContent.main = './index.js';
 packageJsonContent.module = './index.mjs';
 packageJsonContent.types = './index.d.ts';
 await writeFile(join(__dirname, 'dist', 'package.json'), JSON.stringify(packageJsonContent, null, 2));
+console.log('Copied package.json to dist');
+
+// Create empty yarn.lock to pass the build
+const yarnLockPath = join(__dirname, 'dist', 'yarn.lock');
+await writeFile(yarnLockPath, '');
+console.log('Created empty yarn.lock');
 
 
