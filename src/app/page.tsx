f830e78a8d4615916bd612fec57d1a18556c0402
zharinov-nikita <zharinov.nicita@gmail.com>
2025-07-16 13:35:31 +0700

    feat:
    - added a timer update to the json schema
    - fixed the issue related to editorRef.current.layout()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,6 +44,7 @@ export default function Home() {
 		introspectionData,
 		fetchIntrospection,
 		isLoading: isIntrospectionLoading,
+		isReFetching: isIntrospectionReFetching,
 		error: introspectionError,
 	} = useIntrospection(debouncedUrl);
 	const {
@@ -64,13 +65,27 @@ export default function Home() {
 	}, [debouncedUrl, fetchIntrospection]);
 
 	useEffect(() => {
-		if (introspectionData?.procedures.length && !query) {
-			const firstProcedure = introspectionData.procedures[0];
-			const exampleQuery = {
-				procedure: firstProcedure.path,
-				input: getDefaultInputForSchema(firstProcedure.inputSchema),
-			};
-			setQuery(JSON.stringify(exampleQuery, null, 2));
+		if (introspectionData?.procedures.length) {
+			let parsed: { procedure?: string } | null;
+
+			try {
+				parsed = JSON.parse(query);
+			} catch {
+				parsed = null;
+			}
+
+			const validProcedure = introspectionData.procedures.find(
+				(p) => p.path === parsed?.procedure,
+			);
+
+			if (!validProcedure) {
+				const firstProcedure = introspectionData.procedures[0];
+				const exampleQuery = {
+					procedure: firstProcedure.path,
+					input: getDefaultInputForSchema(firstProcedure.inputSchema),
+				};
+				setQuery(JSON.stringify(exampleQuery, null, 2));
+			}
 		}
 	}, [introspectionData, query]);
 
@@ -130,6 +145,9 @@ export default function Home() {
 								introspectionData={introspectionData}
 								onReloadIntrospection={fetchIntrospection}
 								isIntrospectionLoading={isIntrospectionLoading}
+								isIntrospectionReFetching={
+									isIntrospectionReFetching
+								}
 								introspectionError={introspectionError}
 								headers={headers}
 								onHeadersChange={setHeaders}
